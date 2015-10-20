4c2898f7ec40ba56a6991db4034a3406c98b1611
CESAR AUGUSTO MONTAÑEZ <cesaringindustrial@gmail.com>
2015-10-20 16:04:35 -0500

    Cambio de general a mas especifico Bogota

--- public/js/template.js
+++ public/js/template.js
@@ -1,7 +1,8 @@
+var b;
 function inicio()
 {
     var t=document.getElementById("graphic");
-
+    b=document.getElementById("Buttongraphic");
     var textcanvas= t.getContext("2d");
     lienzo = t.getContext("2d");
     textcanvas.font="bold 10px sans-serif";
@@ -26,5 +27,112 @@ function inicio()
 	lienzo.strokeStyle= "rgba(127, 127, 185, 1)"; 
 	lienzo.closePath();
 	lienzo.stroke();
-}
 	
+	b.addEventListener("click", P1100);
+	b.addEventListener("click", P1200);
+	b.addEventListener("click", P1300);
+	b.addEventListener("click", P1400);
+	b.addEventListener("click", P1500);
+	b.addEventListener("click", P1600);
+	b.addEventListener("click", P1700);
+	b.addEventListener("click", P1800);
+	b.addEventListener("click", P1900);
+	b.addEventListener("click", P2000);
+	b.addEventListener("click", P2100);
+	b.addEventListener("click", P2200);
+	b.addEventListener("click", P2300);
+}
+function P1100()
+{
+    lienzo.beginPath();
+	lienzo.moveTo(25,450);
+	lienzo.lineTo(70,400);
+	lienzo.strokeStyle= "rgba(127, 127, 185, 1)"; 
+	lienzo.closePath();
+	lienzo.stroke();
+    
+}
+
+function P1100()
+{
+    lienzo.beginPath();
+	lienzo.moveTo(25,450);
+	lienzo.lineTo(70,400);
+	lienzo.strokeStyle= "rgba(127, 127, 185, 1)"; 
+	lienzo.closePath();
+	lienzo.stroke();
+    
+}
+function P1100()
+{
+    lienzo.beginPath();
+	lienzo.moveTo(25,450);
+	lienzo.lineTo(70,400);
+	lienzo.strokeStyle= "rgba(127, 127, 185, 1)"; 
+	lienzo.closePath();
+	lienzo.stroke();
+    
+}
+function P1100()
+{
+    lienzo.beginPath();
+	lienzo.moveTo(25,450);
+	lienzo.lineTo(70,400);
+	lienzo.strokeStyle= "rgba(127, 127, 185, 1)"; 
+	lienzo.closePath();
+	lienzo.stroke();
+    
+}
+function P1100()
+{
+    lienzo.beginPath();
+	lienzo.moveTo(25,450);
+	lienzo.lineTo(70,400);
+	lienzo.strokeStyle= "rgba(127, 127, 185, 1)"; 
+	lienzo.closePath();
+	lienzo.stroke();
+    
+}
+function P1100()
+{
+    lienzo.beginPath();
+	lienzo.moveTo(25,450);
+	lienzo.lineTo(70,400);
+	lienzo.strokeStyle= "rgba(127, 127, 185, 1)"; 
+	lienzo.closePath();
+	lienzo.stroke();
+    
+}
+function P1100()
+{
+    lienzo.beginPath();
+	lienzo.moveTo(25,450);
+	lienzo.lineTo(70,400);
+	lienzo.strokeStyle= "rgba(127, 127, 185, 1)"; 
+	lienzo.closePath();
+	lienzo.stroke();
+    
+}
+function P1100()
+{
+    lienzo.beginPath();
+	lienzo.moveTo(25,450);
+	lienzo.lineTo(70,400);
+	lienzo.strokeStyle= "rgba(127, 127, 185, 1)"; 
+	lienzo.closePath();
+	lienzo.stroke();
+    
+}
+function P1100()
+{
+    lienzo.beginPath();
+	lienzo.moveTo(25,450);
+	lienzo.lineTo(70,400);
+	lienzo.strokeStyle= "rgba(127, 127, 185, 1)"; 
+	lienzo.closePath();
+	lienzo.stroke();
+    
+}
+
+
+
