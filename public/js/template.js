dc1bdde26d401eb2a3fcebb27f9b45c1868afb92
CESAR AUGUSTO MONTAÑEZ <cesaringindustrial@gmail.com>
2015-10-20 17:15:01 -0500

    agregue el javascript

--- public/js/template.js
+++ public/js/template.js
@@ -1,138 +1,15 @@
 var b;
 function inicio()
 {
-    var t=document.getElementById("graphic");
-    b=document.getElementById("Buttongraphic");
-    var textcanvas= t.getContext("2d");
-    lienzo = t.getContext("2d");
-    textcanvas.font="bold 10px sans-serif";
-    textcanvas.fillStyle= "white";
-    textcanvas.fillText("0",25,475)
-    textcanvas.fillText("1.100",70,475)
-    textcanvas.fillText("1.200",115,475)
-    textcanvas.fillText("1.300",160,475)
-    textcanvas.fillText("1.400",205,475)
-    textcanvas.fillText("1.500",250,475)
-    textcanvas.fillText("1.600",295,475)
-    textcanvas.fillText("1.700",340,475)
-    textcanvas.fillText("1.800",385,475)
-    textcanvas.fillText("1.900",430,475)
-    textcanvas.fillText("2.000",475,475)
-    textcanvas.fillText("2.100",520,475)
-    textcanvas.fillText("2.200",565,475)
-    textcanvas.fillText("2.300",610,475)
-    lienzo.beginPath();
-	lienzo.moveTo(25,450);
-	lienzo.lineTo(625,450);
-	lienzo.strokeStyle= "rgba(127, 127, 185, 1)"; 
-	lienzo.closePath();
-	lienzo.stroke();
-	
-	b.addEventListener("click", P1100);
-	b.addEventListener("click", P1200);
-	b.addEventListener("click", P1300);
-	b.addEventListener("click", P1400);
-	b.addEventListener("click", P1500);
-	b.addEventListener("click", P1600);
-	b.addEventListener("click", P1700);
-	b.addEventListener("click", P1800);
-	b.addEventListener("click", P1900);
-	b.addEventListener("click", P2000);
-	b.addEventListener("click", P2100);
-	b.addEventListener("click", P2200);
-	b.addEventListener("click", P2300);
-}
-function P1100()
-{
-    lienzo.beginPath();
-	lienzo.moveTo(25,450);
-	lienzo.lineTo(70,400);
-	lienzo.strokeStyle= "rgba(127, 127, 185, 1)"; 
-	lienzo.closePath();
-	lienzo.stroke();
-    
-}
+    var Order_A1=document.getElementById("Order-A1");
+    var Delivery_A2=document.getElementById("Delivery-A2");
+    b=document.getElementById("button");
+    b.addEventListener("click", fulfillment);
+ }
 
-function P1100()
-{
-    lienzo.beginPath();
-	lienzo.moveTo(25,450);
-	lienzo.lineTo(70,400);
-	lienzo.strokeStyle= "rgba(127, 127, 185, 1)"; 
-	lienzo.closePath();
-	lienzo.stroke();
-    
-}
-function P1100()
-{
-    lienzo.beginPath();
-	lienzo.moveTo(25,450);
-	lienzo.lineTo(70,400);
-	lienzo.strokeStyle= "rgba(127, 127, 185, 1)"; 
-	lienzo.closePath();
-	lienzo.stroke();
-    
-}
-function P1100()
-{
-    lienzo.beginPath();
-	lienzo.moveTo(25,450);
-	lienzo.lineTo(70,400);
-	lienzo.strokeStyle= "rgba(127, 127, 185, 1)"; 
-	lienzo.closePath();
-	lienzo.stroke();
-    
-}
-function P1100()
-{
-    lienzo.beginPath();
-	lienzo.moveTo(25,450);
-	lienzo.lineTo(70,400);
-	lienzo.strokeStyle= "rgba(127, 127, 185, 1)"; 
-	lienzo.closePath();
-	lienzo.stroke();
-    
-}
-function P1100()
-{
-    lienzo.beginPath();
-	lienzo.moveTo(25,450);
-	lienzo.lineTo(70,400);
-	lienzo.strokeStyle= "rgba(127, 127, 185, 1)"; 
-	lienzo.closePath();
-	lienzo.stroke();
-    
-}
-function P1100()
-{
-    lienzo.beginPath();
-	lienzo.moveTo(25,450);
-	lienzo.lineTo(70,400);
-	lienzo.strokeStyle= "rgba(127, 127, 185, 1)"; 
-	lienzo.closePath();
-	lienzo.stroke();
-    
-}
-function P1100()
-{
-    lienzo.beginPath();
-	lienzo.moveTo(25,450);
-	lienzo.lineTo(70,400);
-	lienzo.strokeStyle= "rgba(127, 127, 185, 1)"; 
-	lienzo.closePath();
-	lienzo.stroke();
-    
-}
-function P1100()
-{
-    lienzo.beginPath();
-	lienzo.moveTo(25,450);
-	lienzo.lineTo(70,400);
-	lienzo.strokeStyle= "rgba(127, 127, 185, 1)"; 
-	lienzo.closePath();
-	lienzo.stroke();
-    
-}
-
-
-
+ function fulfillment(){
+ 	var A1=Number(Order_A1.value);
+ 	var A2=Number(Delivery_A2.value);
+ 	var A3= ((A2-A1)/A1)*100;
+ 	A3 =document.getElementById("Fulfillment-A3");
+ }   
