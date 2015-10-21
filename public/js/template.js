bdf26211ec1322f5c5599dfc8219fa9259c28bce
CESAR AUGUSTO MONTAÑEZ <cesaringindustrial@gmail.com>
2015-10-20 22:26:41 -0500

    formulacion del javascript y cambio en los id

--- public/js/template.js
+++ public/js/template.js
@@ -1,8 +1,8 @@
 var b;
 function inicio()
 {
-    var Order_A1=document.getElementById("Order-A1");
-    var Delivery_A2=document.getElementById("Delivery-A2");
+    var Order_A1=document.getElementById("Order_A1");
+    var Delivery_A2=document.getElementById("Delivery_A2");
     b=document.getElementById("button");
     b.addEventListener("click", fulfillment);
  }
@@ -10,6 +10,17 @@ function inicio()
  function fulfillment(){
  	var A1=Number(Order_A1.value);
  	var A2=Number(Delivery_A2.value);
- 	var A3= ((A2-A1)/A1)*100;
- 	A3 =document.getElementById("Fulfillment-A3");
- }   
+ 
+
+ 	
+ 	if(A1>"" && A2>""){
+	var A3= (A2/A1)*100;
+	
+	document.getElementById("Fulfillment_A3").value=A3+"%";
+ 	}
+}
+	
+	
+
+
+  
